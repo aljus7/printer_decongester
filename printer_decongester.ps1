15143417709c4ff481553e85edbd97d482b1bfb3
aljosa mlinaric <mlinari.aljosa@gmail.com>
2025-03-18 12:43:42 +0100

    Added logic for closing processes that own files.

--- printer_decongester.ps1
+++ printer_decongester.ps1
@@ -7,6 +7,7 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     }
 }
 
+$filesPrint = Get-ChildItem -Path "C:\Windows\System32\spool\PRINTERS\" -Recurse
 Write-Host "SHELL WILL CLOSE AUTOMATICALLY!!" -ForegroundColor Yellow
 
 $retry = 3
@@ -14,6 +15,24 @@ while($retry -gt 0) {
     $can_break = $false
     Stop-Service -Name Spooler -ErrorAction SilentlyContinue  
     if ($?) {
+        if ($retry -lt 2) {
+            if($retry -eq 2) {
+                Write-Output "Taking drastic measures, closing processes that own files!" -ForegroundColor Yellow
+            }
+            foreach ($file in $filesPrint) {
+                $output = handle -accepteula $file.FullName
+
+                if ($output -match "pid: (\d+)") {
+                    $pid = $matches[1]
+                    Write-Output "File: $($file.FullName) is being used by Process ID: $pid" -ForegroundColor Yellow
+
+                    Stop-Process -Id $pid -Force
+                    Write-Output "Terminated Process ID: $pid" -ForegroundColor Orange
+                } else {
+                    Write-Output "File: $file is not used by any process" -ForegroundColor Yellow
+                }
+            }
+        }
         Start-Sleep -Seconds 5
         Remove-Item -Path "C:\Windows\System32\spool\PRINTERS\*" -Force
         if ($?) {
@@ -40,6 +59,12 @@ while($retry -gt 0) {
     $retry--
  }
 
+ if ($can_break -eq $true) {
+    Write-Host "All done, printers should work again now!" -ForegroundColor Green
+ }
+
  if ($retry -eq 0) {
     Write-Host "All retrys where unsucessfull :(" -ForegroundColor Red
 }
+
+Start-Sleep -Seconds 3
